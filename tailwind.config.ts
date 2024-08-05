51e1811211bf41f0ead64e0cd836db1e8370117d
brosing <singgihnn@gmail.com>
2024-08-05 17:33:55 +0700

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,21 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)'
+          }
+        }
       },
+      animation: {
+        'border-spin': 'border-spin 2s linear infinite'
+      }
     },
   },
   plugins: [],
